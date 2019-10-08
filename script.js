cd5cd7eb6cc5d7451caf50a598e813685ac29eae
oltionap0876 <56265208+oltionap0876@users.noreply.github.com>
2019-10-08 11:05:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Oltiona"
+let userName = "oltionap0876";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
